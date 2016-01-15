efd5be4cc79bf8de7c46d8e99090aa740c85e54e
sumitee2 <sumit.einstein@gmail.com>
2016-01-15 13:52:07 +0530

    initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sumitchaudhary/workspace/moldedbits/mb-appstore/djangoAppStore/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
