b1b93b2cd4a79825cc0e22a95ddd22ba532101ef
sumitee2 <sumit.einstein@gmail.com>
2016-01-19 14:51:46 +0530

    connected celery with aws

--- mbApp/ios_app_list/tasks.py
+++ mbApp/ios_app_list/tasks.py
@@ -2,6 +2,9 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 from .models import AppResource
+import boto3
+import botocore
+
 #from photos.utils import save_latest_flickr_image
 
 logger = get_task_logger(__name__)
@@ -9,15 +12,25 @@ logger = get_task_logger(__name__)
 
 @periodic_task(
     run_every=(crontab(minute='*/1')),
-    name="fetch_contents",
+    name="iterate_bucket",
     ignore_result=True
 )
-def fetch_contents():
-    """
-    Fetches content from aws server
-    """
-    all_app_resources = AppResource.objects.all()
-    count = len(all_app_resources)
-    a_r = AppResource(resource_key = 'resource num %d' %count)
-    a_r.save()
-    logger.info("Content from aws")
+def iterate_bucket():
+    AppResource.objects.all().delete()
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('mb-appstore')
+    exists = True
+    try:
+        s3.meta.client.head_bucket(Bucket='mb-appstore')
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = int(e.response['Error']['Code'])
+        if error_code == 404:
+            exists = False
+    if exists:
+        for key in bucket.objects.all():
+            a_r = AppResource(resource_key = '%s' %key.key)
+            a_r.save()
+
+
